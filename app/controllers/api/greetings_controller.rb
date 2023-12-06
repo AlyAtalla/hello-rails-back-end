a207e0c011f1c9298056a11402797ac225aa0cb1
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-06 09:49:31 +0200

    update greeting controller

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -3,7 +3,7 @@ module Api
   class GreetingsController < ApplicationController
     def random
       @greeting = Message.order('RANDOM()').first
-      render json: { greeting: @greeting&.message }
-    end
+      render json: { greeting: @greeting&.greeting }
+    end   
   end
 end
